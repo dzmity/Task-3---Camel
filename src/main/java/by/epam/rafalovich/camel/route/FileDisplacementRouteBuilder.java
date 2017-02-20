7f4cf7c2eaec9264ee268badc35b4718564e4503
Dzmitry Rafalovich <dzmitry_rafalovich@epam.com>
2017-02-20 16:17:16 +0300

    create handler for routes

--- src/main/java/by/epam/rafalovich/camel/route/FileDisplacementRouteBuilder.java
+++ src/main/java/by/epam/rafalovich/camel/route/FileDisplacementRouteBuilder.java
@@ -1,7 +1,11 @@
 package by.epam.rafalovich.camel.route;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.builder.xml.Namespaces;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 /**
@@ -10,11 +14,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class FileDisplacementRouteBuilder extends RouteBuilder {
 
+    private static final Logger LOG = LoggerFactory.getLogger(FileDisplacementRouteBuilder.class);
+
     private static final Namespaces ns3 =
             new Namespaces("ns3", "http://atp.ihg.com/schema/common/utility/communication/datatypes/v5");
 
     @Override
     public void configure() throws Exception {
+
+        onException(Exception.class).process(new Processor() {
+
+            public void process(Exchange exchange) throws Exception {
+                LOG.error("*****Exception occurs!");
+            }
+        }).log("*****Handle exception in route ${routeId}. Received body ${body}").handled(true);
 
         from("file://src/main/resources?fileName=file.xml")
                 .log("*****Displacement file.xml to the inprogress folder")
@@ -34,6 +47,6 @@ public class FileDisplacementRouteBuilder extends RouteBuilder {
                 .setHeader("subject", constant("Process completion notification"))
                 .transform().simple("All operations was successfully done. ${in.body}")
                 .log("***** Send EMAIL to dzmitry_rafalovich@epam.com from subscriber queue.")
-                .to("smtps://smtp.gmail.com?username=work.d.rafalovich@gmail.com&password=epam2016&to=dzmitry_rafalovich@epam.com");
+                .to("smtps://smtp.gmail.com?username=work.d.rafalovich@gmail.com&password=******&to=dzmitry_rafalovich@epam.com");
     }
 }
